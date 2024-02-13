28d92f9e263d8f55d30a9d2592646fc601748423
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2024-02-13 06:31:15 +0000

    Edited calculate_server

--- server_code/calculate_server.py
+++ server_code/calculate_server.py
@@ -8,14 +8,15 @@ from anvil.tables import app_tables
 import anvil.server
 import datetime
 @anvil.server.callable
-def calculate_next_month_due(scheduled_payment):
+def calculate_next_month_due():
+  app_tables.fin_loan_details.search()
     # Assuming scheduled_payment is a string in the format 'YYYY-MM-DD'
-    try:
-        scheduled_date = datetime.datetime.strptime(scheduled_payment, '%Y-%m-%d').date()
-        next_due_date = scheduled_date + relativedelta(months=1)
-        return next_due_date.strftime('%Y-%m-%d')
-    except ValueError:
-        return None
+  try:
+      scheduled_date = datetime.datetime.strptime(scheduled_payment, '%Y-%m-%d').date()
+      next_due_date = scheduled_date + relativedelta(months=1)
+      return next_due_date.strftime('%Y-%m-%d')
+  except ValueError:
+      return None
 
 @anvil.server.callable
 def today_dues():
